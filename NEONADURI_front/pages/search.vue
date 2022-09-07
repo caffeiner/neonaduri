3616e4d64961c7740c1b53043400718d3d5fc34d
ksh3967 <ksh3967@naver.com>
2022-09-07 15:17:35 +0900

    [S07P22A702-29] feat : review page

--- NEONADURI_front/pages/search.vue
+++ NEONADURI_front/pages/search.vue
@@ -2,7 +2,23 @@
 <template>
   <div class="search">
     <div class="main-content">
-      <div>추천</div>
+      <div class="recommand">
+        <div class="recommand-content">
+          <img
+            src="@/static/logo/logo-black.png"
+            alt="임시"
+            style="width: 200px"
+          />
+          <div>
+            <div>동궁과 월지</div>
+            <div>어쩌고 저쩌고</div>
+            <div>#데이트 #경주</div>
+          </div>
+          <div>
+            <div class="top-title">추천장소</div>
+          </div>
+        </div>
+      </div>
       <div>검색</div>
       <div>지역</div>
       <div>표</div>
@@ -23,11 +39,25 @@ export default {
 .search {
   width: 100vw;
   height: 100vh;
-  background-color: #eaf2f9;
   display: flex;
   justify-content: space-between;
 }
 .main-content {
   width: 85vw;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+.recommand {
+  width: 70vw;
+  background-color: #eaf2f9;
+}
+.recommand-content {
+  display: flex;
+  justify-content: space-between;
+  background-color: #cbe7f0;
+}
+.top-title {
+  background-color: #cbdcf0;
 }
 </style>
