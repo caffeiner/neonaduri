ea899c9e6e7969ea8e29e9f334a0560da17c07bb
박종찬 <whdcks5681@naver.com>
2022-09-07 14:56:18 +0900

    feat: essential package init

---
+++ NEONADURI_back/src/main/java/neonaduri/api/repository/ReviewRepository.java
@@ -0,0 +1,9 @@
+package neonaduri.api.repository;
+
+import neonaduri.domain.Review;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
+
+@Transactional(readOnly = true)
+public interface ReviewRepository extends JpaRepository<Review, Long> {
+}
