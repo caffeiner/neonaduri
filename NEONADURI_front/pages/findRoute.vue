4b1918e1861b2f71c01f1a956db7f7e015cc795e
ksh3967 <ksh3967@naver.com>
2022-09-13 14:46:23 +0900

    [S07P22A702-41] feat: review write page add

--- NEONADURI_front/pages/findRoute.vue
+++ NEONADURI_front/pages/findRoute.vue
@@ -1,126 +1,120 @@
 <template>
   <div class="findRoute">
     <div class="container">
-        <div class="banner">
-            <img src="/banner/banner.png" style="width: 50%;">
+      <div class="banner">
+        <img src="/banner/banner.png" style="width: 50%" />
+      </div>
+      <div class="search">
+        <div class="input-group mb-3">
+          <input
+            type="text"
+            class="form-control input-form"
+            aria-label="Default"
+            aria-describedby="inputGroup-sizing-default"
+            placeholder="출발지 입력"
+          />
         </div>
-        <div class="search">
-            <div class="input-group mb-3">
-                <input type="text" class="form-control input-form" aria-label="Default" aria-describedby="inputGroup-sizing-default" placeholder="출발지 입력">
-            </div>
-            <div class="input-group mb-3">
-                <input type="text" class="form-control input-form" aria-label="Default" aria-describedby="inputGroup-sizing-default" placeholder="도착지 입력">
-            </div>
+        <div class="input-group mb-3">
+          <input
+            type="text"
+            class="form-control input-form"
+            aria-label="Default"
+            aria-describedby="inputGroup-sizing-default"
+            placeholder="도착지 입력"
+          />
         </div>
-        <div class="my-button">
-            <div class="types reInput">
-                
-                <div class="buttons">
-                    다시입력
-                </div>
-            </div>
-            <div class="types roadFinding">
-                <div class="buttons">
-                    길찾기     >
-                </div>
-                
-            </div>
+      </div>
+      <div class="my-button">
+        <div class="types reInput">
+          <div class="buttons">다시입력</div>
         </div>
-        <div class="roadMap">
-
-            
-
+        <div class="types roadFinding">
+          <div class="buttons">길찾기 ></div>
         </div>
-
-
+      </div>
+      <div class="roadMap"></div>
     </div>
   </div>
 </template>
 
-<script>
-export default {
-    
-}
+<script scoped>
+export default {}
 </script>
 
 <style>
-    .findRoute{
-        background-color: #bdd9e5;
-        width: 100%;
-        height: 100vh;
-    }
+.findRoute {
+  background-color: #bdd9e5;
+  width: 100%;
+  height: 100vh;
+}
 
-    .container{
-        display:inline;
-    }
+.container {
+  display: inline;
+}
 
-    .banner{
-        display: flex;
-        align-items: center;
-        justify-content: center;
-    }
+.banner {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
 
-    .input-form{
-        width: 18vw;
-        height: 6vh;
-        font-size: larger;
-        margin-top: 3vh;
-        margin-bottom: 3vh;
-        margin-left: 4vw;
-        margin-right: 4vw;
-    }
-    
-    .search{
-        display: flex;
-        align-items: center;
-        justify-content: center;
-    }
+.input-form {
+  width: 18vw;
+  height: 6vh;
+  font-size: larger;
+  margin-top: 3vh;
+  margin-bottom: 3vh;
+  margin-left: 4vw;
+  margin-right: 4vw;
+}
 
-    .my-button{
-        display: flex;
-        align-items: center;
-        justify-content: center;
-    }
+.search {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
 
-    .types{
-        width: 18vw;
-        height: 6vh;
-        margin-top: 1vh;
-        margin-bottom: 3vh;
-        margin-left: 4vw;
-        margin-right: 4vw;
-    }
+.my-button {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
 
-    .reInput{
-        display:flex;
-        justify-content: flex-start;
-    }
+.types {
+  width: 18vw;
+  height: 6vh;
+  margin-top: 1vh;
+  margin-bottom: 3vh;
+  margin-left: 4vw;
+  margin-right: 4vw;
+}
 
-    .roadFinding{
-        display:flex;
-        justify-content: flex-end;
-    }
+.reInput {
+  display: flex;
+  justify-content: flex-start;
+}
 
-    .buttons{
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        width:5vw;
-        border-radius: 20px;
-        border: 1px solid white;
-        float: left;
-        box-shadow: 3px 3px 3px black;
+.roadFinding {
+  display: flex;
+  justify-content: flex-end;
+}
 
-        transition-duration: 0.3s;
-    }
+.buttons {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 5vw;
+  border-radius: 20px;
+  border: 1px solid white;
+  float: left;
+  box-shadow: 3px 3px 3px black;
 
-    .buttons:active{
-        margin-left: 5px;
-        margin-top:5px;
-        box-shadow: none;
-    }
+  transition-duration: 0.3s;
+}
 
-
-   
-    
+.buttons:active {
+  margin-left: 5px;
+  margin-top: 5px;
+  box-shadow: none;
+}
 </style>
