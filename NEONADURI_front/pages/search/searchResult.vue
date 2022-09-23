b767c5f7457bdb314e9c4b7bb40a75d6741dbaa8
ksh3967 <ksh3967@naver.com>
2022-09-23 10:19:10 +0900

    [S07P22A702-64] review and route store

--- NEONADURI_front/pages/search/searchResult.vue
+++ NEONADURI_front/pages/search/searchResult.vue
@@ -129,7 +129,7 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
 @font-face {
   font-family: 'GmarketSansMedium';
   src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2001@1.1/GmarketSansMedium.woff')
