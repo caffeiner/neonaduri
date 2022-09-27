a3004aaeaf55758cd90882ff402cb27b48c82606
sunho1 <87847307+sunho1@users.noreply.github.com>
2022-09-27 10:45:27 +0900

    commit

--- NEONADURI_front/nuxt.config.js
+++ NEONADURI_front/nuxt.config.js
@@ -60,7 +60,13 @@ export default {
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
     baseURL: '/',
+    proxy:true
   },
+
+  proxy:{
+    '/data/':'https://echarts.apache.org/examples'
+  },
+
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {},
