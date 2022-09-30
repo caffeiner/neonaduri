79b6398b343c45e80a0e2527dc37f3198df5e015
ksh3967 <ksh3967@naver.com>
2022-09-30 10:04:33 +0900

    [S07P22A702-77] Fix password bug

--- NEONADURI_back/src/main/java/neonaduri/dto/request/ModifyReviewReq.java
+++ NEONADURI_back/src/main/java/neonaduri/dto/request/ModifyReviewReq.java
@@ -13,8 +13,6 @@ public class ModifyReviewReq {
 
     @NotNull
     private final Long reviewId;
-    @NotNull
-    private final MultipartFile reviewImage;
 
     @NotNull
     private final String reviewContent;
@@ -22,9 +20,8 @@ public class ModifyReviewReq {
     private final String tags;
 
     @Builder
-    public ModifyReviewReq(Long reviewId, MultipartFile reviewImage, String reviewContent, String tags) {
+    public ModifyReviewReq(Long reviewId, String reviewContent, String tags) {
         this.reviewId = reviewId;
-        this.reviewImage = reviewImage;
         this.reviewContent = reviewContent;
         this.tags = tags;
     }
