4239bbeb1995f0170aa59548c36d6c6b20ce344a
ksh3967 <ksh3967@naver.com>
2022-10-05 15:45:06 +0900

    fix maincard font

--- NEONADURI_front/pages/index.vue
+++ NEONADURI_front/pages/index.vue
@@ -17,13 +17,13 @@
       <div class="main-card-box">
         <div class="cards" @click="moveSearch">
           <div class="main-card">
-            <img src="/maincard/main-search-button.png" class="card-img" />
+            <img src="/maincard/search-button.png" class="card-img" />
             <img src="/maincard/main-move.png" alt="" class="card-img back" />
           </div>
         </div>
         <div class="cards" @click="moveStatistics">
           <div class="main-card">
-            <img src="/maincard/main-statistics-button.png" class="card-img" />
+            <img src="/maincard/statistics-button.png" class="card-img" />
             <img src="/maincard/main-move.png" class="card-img back" />
           </div>
         </div>
